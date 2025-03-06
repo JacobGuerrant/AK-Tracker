3392e830e2b0383d5e48e4533fe84ae0920779de
Jacob <jacobguerrant01@gmail.com>
2025-03-06 14:56:07 -0500

    Partial Navbar and User Auth implementation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode}>) {
   return (
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           <Navbar />
+
           {children}
         </body>
       </html>
